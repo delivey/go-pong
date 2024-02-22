5b01bfaa93a5a2e31e833d46e708cf0fbf3a8f62
dev <deliveydev@gmail.com>
2024-02-22 09:47:57 +0200

    Basic graphics, ball movement

--- main.go
+++ main.go
@@ -1,30 +1,68 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-type Game struct{}
+var SCREEN_WIDTH = 320
+var SCREEN_HEIGHT = 240
+
+type Position struct {
+	X, Y float32
+}
+
+type Ball struct {
+	Position
+	Radius float32
+}
+
+type Game struct {
+	Ball        Ball
+	CurrentTurn int
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
+func (g *Game) HandleBall(screen *ebiten.Image) {
+	g.Ball.X += 2.5
+	if (g.Ball.X) > float32(SCREEN_WIDTH) {
+		g.Ball.X = float32(SCREEN_WIDTH) / 2
+	}
+	vector.DrawFilledCircle(screen, g.Ball.X, g.Ball.Y, g.Ball.Radius, color.RGBA{255, 255, 255, 255}, false)
+}
+
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	// Left player
+	vector.DrawFilledRect(screen, 20, 50, 6, 30, color.RGBA{255, 255, 255, 255}, true)
+
+	g.HandleBall(screen)
+
+	// Right player
+	vector.DrawFilledRect(screen, 294, 50, 6, 30, color.RGBA{255, 255, 255, 255}, true)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return SCREEN_WIDTH, SCREEN_HEIGHT
+}
+
+func (g *Game) Init() {
+	g.Ball.X = float32(SCREEN_WIDTH) / 2
+	g.Ball.Y = float32(SCREEN_HEIGHT) / 2
+	g.Ball.Radius = 3
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowSize(960, 720)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	game := &Game{}
+	game.Init()
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
